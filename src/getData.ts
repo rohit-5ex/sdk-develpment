28d9b0ed5e71a229ca7b27214db1f5cc9c1ba303
Hemantu <hkmr22012@gmail.com>
2023-01-04 14:31:46 +0530

    add get data and post data function

---
+++ src/getData.ts
@@ -0,0 +1,14 @@
+import fetch from "node-fetch";
+export async function getData()
+{
+    await fetch("https://jsonplaceholder.typicode.com/posts/1")
+        .then((response) =>  response.json())
+        .then((data) => {
+            // console.log(data); 
+            return data;})
+        .catch((e:any) => {
+               return e;
+            })
+               
+}
+// export default getData;
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,8 @@
-export const Greeter = (name: string) => `Hello ${name}`;
+import {postData} from "./postData";
+import {getData} from "./getData";
+export const Greeter = (name: string) => `Hello ${name}`; 
+postData("This is a title","This is a body",101);
+getData();
+// console.log(Greeter("Hemant"));
+
+
