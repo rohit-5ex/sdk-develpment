cf3db92b3687c8252b4a91e0cb06d9984547b004
Hemantu <hkmr22012@gmail.com>
2023-01-04 14:51:07 +0530

    change in export

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import { postData } from './postData';
 import { getData } from './getData';
-export const Greeter = (name: string) => `Hello ${name}`;
+const Greeter = (name: string) => `Hello ${name}`;
 postData('This is a title', 'This is a body', 101);
 getData();
 // console.log(Greeter("Hemant"));
+export {Greeter,postData,getData}
